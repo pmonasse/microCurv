f5780c415e25256e80ed598086f6bba032d777c3
Pascal Monasse <monasse@imagine.enpc.fr>
2012-05-11 14:16:26 +0200

    Simplification

--- levelLine.cpp
+++ levelLine.cpp
@@ -41,16 +41,15 @@ std::ostream& operator<<(std::ostream& str, const LevelLine& l) {
 /// A dual pixel, square whose vertices are 4 data points
 struct DualPixel {
     DualPixel(Point& p, float l, const unsigned char* im, size_t w);
+    void follow(Point& p, float l, std::vector<Point>& line);
     bool mark_visit(std::vector<bool>& visit) const;
-    void follow(std::vector<bool>& visit, float l, Point& p,
-                std::vector<Point>& line);
     unsigned char level[4];
     Point vertex[4];
 private:
     const unsigned char* _im;
     const size_t _w;
     Dir _d;
-    void complete();
+    void update(bool left, bool right);
     float numSaddle() const; ///< Numerator of saddle value
     float denomSaddle() const; ///< Denominator of saddle value
     bool find_corner(Point& p, float l) const;
@@ -74,15 +73,24 @@ DualPixel::DualPixel(Point& p, float l, const unsigned char* im, size_t w)
     p += linear(level[0],l,level[3])*delta[_d+1]; // Safe: delta[4]==delta[0]
 }
 
-// Complete init by filling points 1 and 2, assuming 0 and 3 are already set.
-void DualPixel::complete() {
-    Dir d=_d, base=0;
-    Point p=vertex[base];
-    for(int i=0, base=0; i<2; i++) {
+// Update fields vertex and level, knowing whether we turn.
+void DualPixel::update(bool left, bool right) {
+    int base= (right? 0: (left? 2:1));
+    if(! right) {
+        vertex[0] = vertex[base];
+        level[0] = level[base];
+    }
+    if(! left) {
+        vertex[3] = vertex[++base];
+        level[3] = level[base];
+    }
+    Dir d=_d;
+    Point p=vertex[0];
+    for(int i=1; i<=2; i++) {
         p += delta[d];
         if(++d==4) d=0;
-        level[++base] = _im[(size_t)p.y*_w+(size_t)p.x];
-        vertex[base] = p;
+        level[i] = _im[(size_t)p.y*_w+(size_t)p.x];
+        vertex[i] = p;
     }
 }
 
@@ -141,14 +149,11 @@ bool DualPixel::mark_visit(std::vector<bool>& visit) const {
 
 /// Find exit point of level line in dual pixel entering at \a p.
 /// Return false if it closes the level line.
-void DualPixel::follow(std::vector<bool>& visit, float l, Point& p,
-                       std::vector<Point>& line) {
-    line.push_back(p);
-    complete();
+void DualPixel::follow( Point& p, float l, std::vector<Point>& line) {
+    assert(level[0]<l && l<level[3]);
     Point c(p);
     if( find_corner(c,l) )
         line.push_back(c);
-    assert(level[0]<l && l<level[3]);
     bool right= (l<level[1]);
     bool left = (level[2]<l);
     if(left && right) { // saddle point: test l<saddle_level without division
@@ -156,24 +161,11 @@ void DualPixel::follow(std::vector<bool>& visit, float l, Point& p,
         right = (denom>0)? (l*denom<num): (l*denom>num);
         left = !right;
     }
-    int base=1;
-    if(left) {
-        if(++_d==4) _d=0;
-        base = 2;
-    } else if(right) {
-        if(--_d<0) _d=3;
-        base = 0;
-    }
-    float coord = linear(level[base], l, level[base+1]);
-    p = vertex[base] + coord*delta[_d+1]; // Safe: delta[4]==delta[0]
-    if(! right) {
-        vertex[0] = vertex[base];
-        level[0] = level[base];
-    }
-    if(! left) {
-        vertex[3] = vertex[++base];
-        level[3] = level[base];
-    }
+    if(left  && ++_d>3) _d=0;
+    if(right && --_d<0) _d=3;
+    update(left,right);
+    float coord = linear(level[0], l, level[3]);
+    p = vertex[0] + coord*delta[_d+1]; // Safe: delta[4]==delta[0]
 }
 
 /// Extract line at level l
@@ -181,9 +173,11 @@ static void extract(const unsigned char* data, size_t w,
                     std::vector<bool>& visit, float l,
                     Point p, std::vector<Point>& line) {
     DualPixel dual(p, l, data, w);
-    do
-        dual.follow(visit, l, p, line);
-    while( dual.mark_visit(visit) );
+    for(bool cont=true; cont;) {
+        line.push_back(p);
+        cont = dual.mark_visit(visit);
+        dual.follow(p,l,line);
+    }
     const Point delta(.5f, .5f);
     for(std::vector<Point>::iterator it=line.begin(); it!=line.end(); ++it)
         *it += delta;
